651a11007d40e44cab412d8d672060ac381d6d7e
Lukas S <172839@protonmail.com>
2018-12-15 12:23:15 +0100

    added forumcontroller, forumindexvm and forum/index

---
+++ Forum/Controllers/ForumController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Forum.MVC.Controllers
+{
+  [Route("")]
+  [Route("Forum")]
+    public class ForumController : Controller
+    {
+      [Route("")]
+      [Route("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
