940653f0b9da9c5b24c5abe8a692764e468f41df
Lukas S <172839@protonmail.com>
2018-12-29 19:33:48 +0100

    minor bug fixed

--- Forum/Program.cs
+++ Forum/Program.cs
@@ -7,17 +7,16 @@ namespace Forum {
       CreateWebHostBuilder(args).Build().Run();
     }
 
-    //TODO: Add custom messages to AccessDenied (Use TempData).
-    //TODO: When going to specific post id, make it so you go there with pagination enabled. Now you can only go to the first page.
-    //TODO: Relocate the edit/lock/delete buttons that are showing beside the thread/topic names.
-    //TODO: There are som redundant ReturnUrl in code. Remove or use!
-    //TODO: Add confirmations when adding, deleting, editing, etc. Maybe modal?
     //TODO: Add visual indicator in Topic/Thread/Post that it is blocked. - beside the name
+    //TODO: Relocate the edit/lock/delete buttons that are showing beside the thread/topic names.
     //TODO: Create Partial views.
+    //TODO: When going to specific post id, make it so you go there with pagination enabled. Now you can only go to the first page.
+    //TODO: Add confirmations when adding, deleting, editing, etc. Maybe modal?
+    //TODO: Add custom messages to AccessDenied (Use TempData).
+    //TODO: There are som redundant ReturnUrl in code. Remove or use!
     //TODO: Prevent pages add to history, for omitting when back button is pressed. Only "nested" pages - Profile and Account.
     //TODO: Mark aktive page in statistics panel (forumManagement).
     //TODO: Add Typeahead with ajax call for searching.
-    //TODO: Styling
     //TODO: Add seed of Default deleted user if possible?
     //TODO: Add deletion timeout for posts and threads?
     //TODO: Add SuperAdmin?
